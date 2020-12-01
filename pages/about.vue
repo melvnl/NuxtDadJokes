f29deba1a058e32cc1f49d99c94e614fd72fc114
melvnl <melvinlim2000@gmail.com>
2020-12-01 20:47:54 +0700

    Initialize First Commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="">
+      <h1>About DadJokes</h1>
+      <p>This is an app that displays corny dad jokes -credit to Traversy Media</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About The App',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for dad jokes' 
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <h2>Welcome to the best dad jokes app on the web</h2>
+  </div>
+      
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'Welcome To Dad Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for dad jokes' 
+                }
+            ]
+        }
+    }
+}
+</script>
+
+
+.
